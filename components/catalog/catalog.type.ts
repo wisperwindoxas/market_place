658fd6566b264394f330244a9aa2a6ca0a4580d1
windoxaswisper <windoxas@gmail.com>
2023-09-14 07:02:24 +0500

    change some

--- components/catalog/catalog.type.ts
+++ components/catalog/catalog.type.ts
@@ -1,15 +1,13 @@
-import { V4Options } from "uuid"
-
-export interface Irows{
-    id: number
-    numberProduct:string
-    name: string
-    rating: string
-    count: string
-    date: string
-    price: string
+export interface Irows {
+  id: number
+  numberProduct: string
+  name: string
+  rating: string
+  count: string
+  date: string
+  price: string
 }
-export interface Icolumns{
-    id: number
-    label:string
+export interface Icolumns {
+  id: number
+  label: string
 }
